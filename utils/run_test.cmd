31d19c9f110fe307b357591e172166a4c2c30cf3
Ааз <aaz@wekings.local>
2026-09-17 17:21:54 +0300

    Итерация 14: полное наполнение данными, проверка формирования отчётов

--- utils/run_test.cmd
+++ utils/run_test.cmd
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001 >nul
-"C:\Program Files (x86)\1cv8t\8.3.27.1508\bin\1cv8t.exe" ENTERPRISE /F"C:\Bases\InfoBase2" /DisableStartupDialogs /DisableStartupMessages /Execute "C:\Bases\ТестовыеДанные.epf" /Out "C:\Users\User\Documents\1c\WeKings\utils\run_test_log.txt"
+"C:\Program Files (x86)\1cv8t\8.3.27.1508\bin\1cv8t.exe" ENTERPRISE /F"C:\Bases\InfoBase2" /DisableStartupDialogs /DisableStartupMessages /DisableUnsafeOperationProtection /Execute "C:\Bases\ТестовыеДанные.epf" /Out "C:\Users\User\Documents\1c\WeKings\utils\run_test_log.txt"
 echo EXITCODE=%ERRORLEVEL%
